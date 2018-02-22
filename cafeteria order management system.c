e233bb364030c5fa1596eec1e76eea2bf80253ab
Prerana Agrawal <pa.prerana@gmail.com>
2018-02-22 23:21:14 +0530

    Update cafeteria order management system.c

--- cafeteria order management system.c
+++ cafeteria order management system.c
@@ -52,18 +52,19 @@ void highlight(int,int);
 
 void login()
 {
-	int a=0,i=0;
+	int ac=0,i=0;
     char uname[10],c=' '; 
-    char pword[10],code[10];
+    char pword[10];
+    char code[10];
     char user[10]="user";
     char pass[10]="pass";
     do
 {
 	
     printf("\n  \xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\  LOGIN FORM  \xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\   ");
-    printf(" \n\n                  ENTER USERNAME:-");
+    printf(" \n\n                  ENTER YOUR USERNAME:-");
 	scanf("%s", &uname); 
-	printf(" \n\n                  ENTER PASSWORD:-");
+	printf(" \n\n                  ENTER YOUR PASSWORD:-");
 	while(i<10)
 	{
 	    pword[i]=getch();
@@ -92,8 +93,8 @@ void login()
 		
 	}
 }
-	while(a<=2);
-	if (a>2)
+	while(ac<=2);
+	if (ac>2)
 	{
 		printf("\nSorry you have entered the wrong username and password for four times!!!");
 		
@@ -140,9 +141,6 @@ void d_mainmenu()
     char ch;
     const char *menu[]= {"   Calculate Bill","   Order Items","   Edit Items","   Display Orders  ","   Search Orders", "   Delete Orders","   Exit"};
     system("cls");
-//textbackground(11);
-//textcolor(0);
-//_setcursortype(_NOCURSOR);
     window(25,50,20,32);
     gotoxy(33,18);
     printf("CAFE MENU");
@@ -154,12 +152,10 @@ void d_mainmenu()
     curser(7);
 }
 
-void highlight(int num,int count)
+void highlight(int num,int cnt)
 {
     if (num==4)
     {
-        //textbackground(11);
-        //textcolor(0);
         gotoxy(30,23);
         printf("   By Code          ");
         gotoxy(30,24);
@@ -170,7 +166,7 @@ void highlight(int num,int count)
         printf("   Back to main menu");
         //textcolor(0);
         //textbackground(2);
-        switch (count)
+        switch (cnt)
         {
         case 1:
             gotoxy(30,23);
@@ -188,7 +184,9 @@ void highlight(int num,int count)
             gotoxy(30,26);
             printf(" - Back to main menu");
             break;
-        }
+	default:
+		printf("Enter again");
+       }
     }
 
     if(num==7)
@@ -211,7 +209,7 @@ void highlight(int num,int count)
         printf("   Exit           ");
         //textcolor(0);
         //textbackground(2);
-        switch(count)
+        switch(cnt)
         {
         case 1:
             gotoxy (30,23);
@@ -346,123 +344,6 @@ void bill()
     }
     fclose(file);
     getch();
-    d_mainmenu();
-}/*function for editing the menu*/
-void edit()
-{
-    int flag=0,choice;
-    char x[ACS],y[ACS];
-    FILE *file;
-    int size;
-    system("cls");
-    window(20,63,20,46);
-    gotoxy(35,18);
-    printf("EDIT ORDERS");
-    ;
-    gotoxy(25,23);
-    printf("Enter Item Code: ");
-    scanf("%s",x);
-    flag=check(x);
-    if(flag==0)
-    {
-        file=fopen("record.txt","r+b");
-        rewind(file);
-        while (fread(&item,sizeof (item),1,file))
-        {
-            if(strcmp(item.code,x)==0)
-            {
-                //textcolor(0);
-                gotoxy(25,27);
-                printf("name       = %s",item.name);
-                gotoxy(25,28);
-                printf("code       = %s",item.code);
-                gotoxy(25,29);
-                printf("rate       = %g",item.rate);
-                gotoxy(25,30);
-                printf("quantity   = %d",item.quantity);
-                gotoxy(25,32);;
-                printf("Do You Want To Edit This Record?(y/n):");
-                fflush(file);
-                if(toupper(getche())=='Y')
-                {
-                    gotoxy(25,34);
-                    printf("1- Edit the Name ");
-                    gotoxy(25,35);
-                    printf("2- Edit the Code ");
-                    gotoxy(25,36);
-                    printf("3- Edit the Rate ");
-                    gotoxy(25,37);
-                    printf("4- Edit the Quantity ");
-                    gotoxy(25,39);  ;
-                    printf(" Please Enter Your Choice(1, 2, 3, 4) ");
-                    scanf("%d",&choice);
-                    switch(choice)
-                    {
-                    case 1:
-                        system("cls");
-                        window(23,48,20,40);
-                        gotoxy(35,18);
-                        printf("EDIT RECORDS");
-                        gotoxy(25,24);
-                        printf(" Enter New Name: ");
-                        scanf("%s",item.name);
-                        size=sizeof(item);
-                        fseek(file,-size,SEEK_CUR);
-                        fwrite(&item,sizeof(item),1,file);
-                        break;
-                    case 2:
-                        system("cls");
-                        window(23,65,20,40);
-                        gotoxy(35,18);
-                        printf("EDIT RECORDS");
-                        gotoxy(25,24);
-                        c_code(y);
-                        strcpy(item.code,y);
-                        size=sizeof(item);
-                        fseek(file,-size,SEEK_CUR);
-                        fwrite(&item,sizeof(item),1,file);
-                        break;
-                    case 3:
-                        system("cls");
-                        window(23,65,20,40);
-                        gotoxy(35,18);
-                        printf("EDIT RECORDS");
-                        gotoxy(25,24);
-                        printf(" Enter New Rate: ");
-                        scanf("%f",&item.rate);
-                        size=sizeof(item);
-                        fseek(file,-size,SEEK_CUR);
-                        fwrite(&item,sizeof(item),1,file);
-                        break;
-                    case 4:
-                        system("cls");
-                        window(23,65,20,40);
-                        gotoxy(35,18);
-                        printf("EDIT RECORDS");
-                        gotoxy(25,24);
-                        printf(" Enter New Quantity: ");
-                        scanf("%d",&item.quantity);
-                        size=sizeof(item);
-                        fseek(file,-size,1);
-                        fwrite(&item,sizeof(item),1,file);
-                        break;
-                    }
-                    gotoxy(27,30);
-                    printf("--- Item has been Edited---");
-                    break;
-                }
-            }
-        }
-    }
-    if (flag==1)
-    {
-        gotoxy(32,30);
-        printf("Item Does Not Exist.");
-        gotoxy(36,32);
-        printf("PLEASE TRY AGAIN");
-    }
-    getch();
-    fclose(file);
     d_mainmenu();
 }
 /*function to display bill window*/
